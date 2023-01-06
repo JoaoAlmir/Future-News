72f86cfb54119dfea8846ca724b7b841f9a884a6
joaoAlmir <junivion@gmail.com>
2023-01-06 15:35:36 -0300

    add front

---
+++ frontend/future-news/src/App.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+
+      {/* <BrowserRouter>
+        <Routes>
+
+          <Route path="/" element={<PaginaHome />} />
+
+          <Route path="/login/" element={<PaginaLogin/>} />
+        
+        </Routes>
+
+      </BrowserRouter> */}
+
+    </div>
+  );
+}
+
+export default App;
